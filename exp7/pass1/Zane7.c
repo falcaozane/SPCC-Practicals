b6b2a88e8956095aeb93b5e56c6fc103f070d0a3
Zane Falcao <falcaozane@gmail.com>
2024-04-28 22:01:33 +0530

    7 practice

--- exp7/pass1/Zane7.c
+++ exp7/pass1/Zane7.c
@@ -19,4 +19,41 @@ int main(){
     fprintf(fp3, "%d %s %s %s\n", locctr, label, opcode, operand);
 
     //USING opcode
+    fscanf(fp1,"%s %s %s",label,opcode,operand);
+    fprintf(fp3,"%d %s %s %s\n",locctr,label,opcode,operand);
+
+    while (strcmp(opcode,"END")!=0)
+    {
+        if (str(opcode,"DC")==0 || str(opcode,"DS")==0)  
+        {
+            fprintf(fp4,"%s %d\n",label,locctr);
+            fprintf(fp3,"%d %s %s %s\n",locctr,label,opcode,operand);
+            locctr+=4;
+        }
+        else
+        {
+            fscanf(fp2,"%s",mnemonic);
+            fprintf(fp3,"%d %s %s %s",locctr,label,opcode,operand);
+            while (strcmp(mnemonic,"end")!=0)
+            {
+                if (strcmp(opcode,mnemonic)==0)
+                {
+                    locctr+=4;
+                    break;
+                }
+                fscanf(fp2,"%s",mnemonic); 
+            }
+            rewind(fp2);
+        }
+        fscanf(fp1,"%s %s %s %s",label,opcode,operand);
+    }
+
+    fprintf(fp3,"%d %s %s %s",locctr,label,opcode,operand);
+    
+    fclose(fp1);
+    fclose(fp2);
+    fclose(fp3);
+    fclose(fp4);
+
+    return 0;
 }
